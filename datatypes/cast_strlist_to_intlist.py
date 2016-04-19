70f7b0d44ac4e557e6d942bedc7cb5c1024cd5c7
mouli <moulipen@gmail.com>
2016-04-19 22:58:09 +0530

    Casting in List

---
+++ datatypes/cast_strlist_to_intlist.py
@@ -0,0 +1,16 @@
+
+input_list = ["1","2","3","4"]
+
+def using_clause(in_list):
+    out_list = list();
+    [out_list.append(int(elem))for elem in in_list]
+    return out_list
+
+def using_map(in_list):
+    return map(int, in_list)
+
+print using_clause(input_list)
+print sum(using_clause(input_list))
+print using_map(input_list)
+print sum(using_map(input_list))
+print sum(input_list) #TypeError: unsupported operand type(s) for +: 'int' and 'str'
